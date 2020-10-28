224d17b9daf391a54af4e366c6897fa1bf1c5520
sebask <sebaskolster@gmail.com>
2020-10-28 15:14:13 +0200

    done

---
+++ ProblemPool2/Pb1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux;
+            int nr = 0;
+            do
+            {
+                aux = Console.ReadLine();
+                if (aux != "")
+                    if (int.Parse(aux) % 2 == 0)
+                        nr++;
+
+            } while (aux != "");
+            Console.WriteLine(nr);
+
+
+        }
+    }
+}
