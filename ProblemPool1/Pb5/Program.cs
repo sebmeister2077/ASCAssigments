4e839ad69f0880c61bb5ad84be0809f3afea5d6b
sebask <sebaskolster@gmail.com>
2020-10-28 11:08:21 +0200

    done

---
+++ ProblemPool1/Pb5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nr = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            if(k<=0||k>=12)
+                Console.WriteLine("k e negativ sau prea mare");
+            else
+            {    
+                while(k>1)
+                {
+                    nr /= 10;
+                    k--;
+                }
+                 Console.WriteLine(nr%10);
+            }
+        }
+    }
+}
