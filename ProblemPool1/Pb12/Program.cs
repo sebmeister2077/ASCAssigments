bf749956d0dfbe129cc20f9073f67652d694cd45
sebask <sebaskolster@gmail.com>
2020-10-28 11:54:26 +0200

    done

---
+++ ProblemPool1/Pb12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, n,result=0;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            n = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+                if (i % n == 0)
+                    result++;
+            Console.WriteLine(result);
+
+        }
+    }
+}
