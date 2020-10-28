280f65475b2467fd5969fd7292291170603344af
sebask <sebaskolster@gmail.com>
2020-10-28 11:52:23 +0200

    done

---
+++ ProblemPool1/Pb11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            n = int.Parse(Console.ReadLine());
+            while(n>0)
+            {
+                Console.Write(n%10+" ");
+                n /= 10;
+            }
+        }
+    }
+}
