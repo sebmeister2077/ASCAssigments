26ef79fba6c1bfc01d4ba166c77facb6e33d11a3
sebask <sebaskolster@gmail.com>
2020-10-28 12:14:14 +0200

    done

---
+++ ProblemPool1/Pb16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            List<int> lista = new List<int>();
+            a = int.Parse(Console.ReadLine());
+            lista.Add(a);
+            b = int.Parse(Console.ReadLine());
+            lista.Add(b);
+            c = int.Parse(Console.ReadLine());
+            lista.Add(c);
+            d = int.Parse(Console.ReadLine());
+            lista.Add(d);
+            e = int.Parse(Console.ReadLine());
+            lista.Add(e);
+
+            lista.Sort();
+            foreach(int nr in lista)
+                Console.WriteLine(nr);
+
+
+        }
+    }
+}
