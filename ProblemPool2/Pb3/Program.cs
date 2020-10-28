5a1dbb7a21bb57f7d4da0b6f7f392f6fe5eb3749
sebask <sebaskolster@gmail.com>
2020-10-28 21:19:33 +0200

    done

---
+++ ProblemPool2/Pb3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            int s = 0, p = 1;
+            for(int i=1;i<=n;i++)
+            { s += i;p *= i; }
+            Console.WriteLine($"Suma={s}\nProdusul={p}");
+        }
+    }
+}
