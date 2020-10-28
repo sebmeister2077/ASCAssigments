86abec108c83598d96f135891924b2940f4badba
sebask <sebaskolster@gmail.com>
2020-10-28 11:01:22 +0200

    done

---
+++ ProblemPool1/Pb3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            if(n%k==0)
+                Console.WriteLine($"{n} se divide cu {k}");
+            else
+                Console.WriteLine($"{n} nu se divide cu {k}");
+
+        }
+    }
+}
