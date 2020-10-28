bbfdd46f20f952e223e8786c16dfe88726785025
sebask <sebaskolster@gmail.com>
2020-10-28 21:26:55 +0200

    done

---
+++ ProblemPool2/Pb5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,count=0;
+            int[] secv;
+            n = int.Parse(Console.ReadLine());
+            secv = new int[n];
+            for (int i = 0; i < n; i++)
+                if (i == secv[i])
+                    count++;
+            Console.WriteLine(count);
+
+        }
+    }
+}
