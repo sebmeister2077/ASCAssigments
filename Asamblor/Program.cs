cf621b711dd6467abedf54e320804b379c609ef8
sebask <sebaskolster@gmail.com>
2021-02-01 14:14:33 +0200

    .

--- Asamblor/Program.cs
+++ Asamblor/Program.cs
@@ -1,21 +1,179 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Asamblor
 {
     class Program
     {
-        static Dictionary<string, string> dic = new Dictionary<string, string>();
-        static void Initialize()
-        {
-            dic.Add();
-        }
+        static Dictionary<string, string> dictionar;
         static void Main(string[] args)
         {
-            Initialize();
+            string pathOut = "C:/MachineCode.txt";
+            using (StreamWriter sw = File.CreateText(pathOut))
+            { dictionar = new Dictionary<string, string>();
+                InitializareDictionar(ref dictionar);
+                string path = @"C:/Users/Sebas/Source/Repos/sebmeister2077/ASCAssigments/Asamblor/SampleCOde.txt";
+                Console.WriteLine("Please modify(in code) or enter full file path:(type OK if its modified in code)");
+                Console.WriteLine("Note: if the program starts at line 5 please write .org 4(as in the sample code)");
+                string str = Console.ReadLine();
+                if (str != "OK")
+                    path = @str;
+                string[] lines = File.ReadAllLines(path);
+                int beginingLine = FindPseudoCommands(lines, ".begin"), lastLine = FindPseudoCommands(lines, ".end");//acestea vor depinde de .begin si .end
+                int startingAdress = ExtractAdress(lines[FindPseudoCommands(lines, ".org")]);//extracts the adress located after the org pseudo-OP
+                List<Eticheta> listaEtichete = new List<Eticheta>();
+                //initializam toate etichetele (deoarece pe prima linie e pposibil sa fie ceruta valoarea/salt la adresa ultimei etichete
+                for (int i = beginingLine; i < lastLine; i++)
+                {
+                    if (lines[i].Contains(":"))
+                    {
+                        string[] twohalves = new string[2];
+                        twohalves = lines[i].Split(':');
+                        twohalves[0].Trim(' ');
+                        lines[i] = twohalves[1];//am sters eticheta deoarece o avem salvata intr o lista
+                        listaEtichete.Add(new Eticheta(twohalves[0], startingAdress + 1 + i - beginingLine));
+                    }
+                    if (lines.Contains("!"))//stergem comentariul (pt noi)
+                    {
+                        string[] twohalves = new string[2];
+                        twohalves = lines[i].Split('!');
+                        lines[i] = twohalves[0];
+                    }
+                }
+                for (int i = startingAdress; i < lastLine; i++)//beginning translation
+                {
+                    lines[i] = lines[i].Trim(' ');
+                    int index = 0;
+                    string comm = "";
+                    while (!dictionar.ContainsKey(comm) && comm.Length < 6)//cea mai lunga instructiune are 5 litere
+                        comm = comm + lines[i][index++].ToString();//tot adauga cate o litera
+                    if (comm.Length == 6)//nu a gasit nicio instructiune( ori spatiu gol ori un comentariu
+                        continue;
+                    StringBuilder strb = new StringBuilder();
+                    string value;
+                    if (dictionar.TryGetValue(FindMnemonicCategory(comm), out value))//cauta codul binar al categoriei comenzii
+                    #region Translation Into Machine language
+                    {
+                        strb.Append(value);
+                        switch (FindMnemonicCategory(comm))//cele 5 formate de instructiuni
+                        {
+                            case "sethi":
+                                
+                                break;
+                            case "branch":
+                                strb.Append("0");
+                                dictionar.TryGetValue(comm, out value);
+                                strb.Append(value);
+                                dictionar.TryGetValue("branch", out value);
+                                strb.Append(value);
+                                break;
+                            case "call":
+
+                                break;
+                            case "memory":
+                                
+                                break;
+                            case "arithmetic":
+                                
+                                break;
+                        }
+                        #endregion
+                    }
+                }
+            }
+        }
+
+        static int FindPseudoCommands(string[] lines, string keyword)
+        {
+            for (int i = 0; i < lines.Length; i++)
+                if (lines[i].Contains(keyword))
+                    return i;
+            return -1;
+        }
+        static string FindMnemonicCategory(string keyword)
+        {
+            if (keyword == "ld" || keyword == "st")
+                return "memory";
+            if (keyword[0] == 'b')
+                return "branch";
+            if (keyword == "addcc" || keyword == "jumpl"||keyword=="andcc" || keyword == "orcc" || keyword == "orncc" || keyword == "srl")
+                return "arithmetic";
+            if (keyword == "call")
+                return "call";
+            return "sethi";
+
+        }
+        static int ExtractAdress(string line)
+        {
+            line = RemoveSpaces(line);
+            string str="";
+            for (int i = 4; i < line.Length; i++)//if all " " on the left are removed then the adress should be located at index 4
+                if (line[i] < '0' || line[i] > '9')//after org should come
+                    break;
+                else
+                    str = str + line[i].ToString();
+            return int.Parse(str);
+        }
+
+        static string RemoveSpaces(string line)
+        {
+            StringBuilder strb=new StringBuilder();
+            foreach(char c in line)
+            {
+                if (c != ' ')
+                    strb.Append(c.ToString());
+            }
+            return strb.ToString();
+        }
+        static string ReturnBinary(int nr)
+        {
+            string str = "";
+            while(nr>0)
+            {
+                str = (nr % 2).ToString() + str;
+                nr /= 2;
+            }
+            while (str.Length != 5)//2^5 registre
+                str = "0"+str;
+            return str;
+        }
+        static void InitializareDictionar(ref Dictionary<string, string> dictionar)
+        {
+            dictionar.Add("sethi/branch","00");
+            dictionar.Add("call", "01");
+            dictionar.Add("arithmetic", "10");
+            dictionar.Add("memory", "11");
+            dictionar.Add("branch", "010");
+            dictionar.Add("sethi", "100");
+            dictionar.Add("addcc", "010000");
+            dictionar.Add("andcc", "010001");
+            dictionar.Add("orcc", "010010");
+            dictionar.Add("orncc", "010110");
+            dictionar.Add("srl", "100110");
+            dictionar.Add("jumpl", "111000");
+            dictionar.Add("ld", "000000");
+            dictionar.Add("st", "000100");
+            dictionar.Add("be", "0001");
+            dictionar.Add("bcs", "0101");
+            dictionar.Add("bneg", "0110");
+            dictionar.Add("bvs", "0111");
+            dictionar.Add("ba", "1000");
+            
+        }
+    }
+    class Eticheta
+    {
+        public string Name;
+        public int adress;
+        public Eticheta(string nm,int ad)
+        {
+            Name = nm;
+            adress = ad;
         }
     }
 }
---
+++ ProblemPool1/Program.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProblemPool1
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            P18(23457);
+        }
+        static void P1()
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("x={0}",-b/a);
+        }
+        static void P2()
+        {
+            int a, b,c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            int delta = b * b - 4 * a * c;
+            if(delta<0)
+                Console.WriteLine("Nu avem solutie reala.");
+            else
+                if(delta==0)
+                Console.WriteLine("Solutie:{0}",(-b+Math.Sqrt(delta))/(2*a));
+            else
+                Console.WriteLine("Solutii:{0} si {1}", (-b + Math.Sqrt(delta)) / (2 * a), (-b - Math.Sqrt(delta)) / (2 * a));
+        }
+        static bool P3(int n,int k)
+        {
+            return n % k==0;
+        }
+        static bool P4(int an)
+        {
+            return an % 4 == 0;
+        }
+        static void P5(ref int nr,int k)
+        {
+            if (k < 1 || k > nr.ToString().Length)
+            {
+                Console.WriteLine("K invalid");
+                return;
+            }
+            int aux = nr;
+            string aux2 = "";
+            for(int i=0;i<k-1;i++)
+            {
+                aux2 = (aux % 10).ToString() + aux2; ;
+                aux /= 10;
+            }
+            Console.WriteLine(aux%10);
+            aux /= 10;
+            if (aux != 0)
+                aux2 = aux.ToString() + aux2;
+            //inlocuim pe nr cu  nr fara a k-a cifra
+            nr = int.Parse(aux2);
+        }
+        static bool P6(int a,int b,int c)
+        {
+            return a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && a + c > b;
+        }
+        static void Swap(ref int a,ref int b)
+        {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+        static void P8(ref int a,ref int b)
+        {
+            a = a + b;
+            b = a - b;//=a initial
+            a = a - b;//=b initial
+        }
+        static void P9(int n)
+        {
+            for (int i = 1; i <= n / 2; i++)
+                if (n % i == 0)
+                    Console.Write($"{i},");
+            Console.WriteLine(n);
+        }
+        static bool P10(int n)
+        {
+            if (n < 2)
+                return false;
+            if (n == 2)
+                return true;
+            for (int i = 3; i <= n / 3; i += 2)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+        static void P11(int n)
+        {
+            while(n>9)
+            {
+                Console.Write($"{n%10},");
+                n /= 10;
+            }
+            Console.WriteLine(n);
+        }
+        static int P12(int a,int b,int n)
+        {
+            if (a > b)
+                return 0;
+            if ((b - a) % n == 0)
+                if (a % n == 0 || b % n == 0)//daca e [6,12] (6-12)/6=1
+                    return (b-a) / n + 1;
+                else
+                    return (b-a) / n;
+            int aux = 0;
+            if (b-a<n)
+            {
+                for (int i = a + 1; i <= b; i++)
+                    if (i % n == 0)
+                        return 1;
+                return 0;
+            }
+            aux += P12(b - b % n, b, n);//facem recursiv pt intervalele mai mici(de lungime maxima n)
+            aux += P12(a-1, a + (n - a % n - 1), n);
+            aux = Math.Min(aux, 1);
+            return (b-a) / n + aux;
+        }
+        static int P13(int y1,int y2,bool firststep=true)
+        {
+            if(firststep)
+            {
+                //nu se pun anii bisecti chiar la margine(zice intre)
+                if (y1 % 4 == 0)
+                    y1++;
+                if (y2 % 4 == 0)
+                    y2--;
+            }
+            if (y2 - y1 <= 4)
+                if ((y1 % 4 >= y2 % 4&&y2!=y1)||y1%4==0||y2%4==0)//anul dinaintea anului bisect are modulo 4 cel mai mare
+                    return 1;
+                else
+                    return 0;
+            else
+                return (y2 - y1) / 4 + Math.Min(1, P13(y1, y1 + (4 - y1 % 4)-1,false) + P13(y2 - y2 % 4, y2,false));
+        }
+        static bool P14(int n)
+        {
+            int aux = n,ogl=0;
+            while(aux>0)
+            {
+                ogl = ogl * 10 + aux % 10;
+                aux /= 10;
+            }
+            return ogl == n;
+        }
+        static void P15(int a,int b,int c)
+        {
+            Console.WriteLine(Math.Min(a,Math.Min(b,c)));
+            Console.WriteLine(Math.Min(Math.Min(Math.Max(a,b),Math.Max(a,c)),Math.Max(b,c)));
+            Console.WriteLine(Math.Max(a,Math.Max(b,c)));
+        }
+        static int FindMin(int a,int b,int c=int.MaxValue,int d=int.MaxValue,int e=int.MaxValue)
+        {
+            return Math.Min(Math.Min(a, b), Math.Min(c, Math.Min(d, e)));
+        }
+        static void P16(int a,int b,int c,int d,int e)
+        {
+            int min=FindMin(a, b, c, d, e);
+            Console.WriteLine(min);
+            a = a == min ? e : a;
+            b = b == min ? e : b;
+            c = c == min ? e : c;
+            d = d == min ? e : d;
+            min = FindMin(a, b, c, d);
+            Console.WriteLine(min);
+            a = a == min ? d : a;
+            b = b == min ? d : b;
+            c = c == min ? d : c;
+            min = FindMin(a, b, c);
+            Console.WriteLine(min);
+            a = a == min ? c : a;
+            b = b == min ? c : b;
+            Console.WriteLine(Math.Min(a,b));
+            Console.WriteLine(Math.Max(a,b));
+        }
+        static int cmmdc(int a,int b)
+        {
+            while (b > 0)
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            return a;
+        }
+        static void P17(int a,int b)
+        {
+            Console.WriteLine($"cmmdc:{cmmdc(a,b)}");
+            Console.WriteLine($"cmmmc:{a*b/cmmdc(a,b)}");
+        }
+        static void P18(int n)
+        {
+            int putere = 0,lastprime=2;
+            while(n>1)
+            {
+                for(int i=lastprime;i<=n;i++)
+                    if(n%i==0)
+                    {
+                        if (i != lastprime && putere > 0)
+                        {
+                            Console.Write($"{lastprime}^{putere}x");
+                            putere = 0;
+                        }
+                        lastprime = i;
+                        putere++;
+                        n /= i;
+                        break;//breaks the for
+                    }
+            }
+            Console.WriteLine($"{lastprime}^{putere}");
+        }
+        static bool P19(int n)
+        {
+            char a='-', b='-';
+            foreach (char c in n.ToString())//iteram prin cifre
+                if (a == '-')
+                    a = c;
+                else
+                    if (c != a)
+                    if (b == '-')
+                        b = c;
+                    else
+                        if(c!=b)
+                        return false;
+            return true;
+        }
+        static void P20(int m,int n)
+        {
+            //https://github.com/sebmeister2077/ASCAssigments/blob/main/Conversii/Program.cs
+            // de la linia 156 ,char[] formareDec o sa fie numarul rezultat de ex 0.033333333...
+            //dupa care verifica SECVENTELE care se repeta si va insera parantezele la locul lor potrivit(cele 2 whileuri de pe linia 203 ai 209): 0.0(3); noua variabila cu perioada va fi inCaseOfProb
+            //desigur precizia se putea modifica ca si in conversia din baza 10(a doua jumatate,linia 257) unde am pus sa gaseasca parioada in primele 2000 de zecimale
+        }
+        static void P20()
+        {
+            //create random nr;
+            int nr = rnd.Next() % 1024 +1;
+            bool reia = true;
+            while(reia)
+            {
+                Console.WriteLine("A-Numarul este mai mare sau egal decat...?");
+                Console.WriteLine("B-Numarul este divizibil cu....?");
+                Console.WriteLine("R-Numarul este egal cu....?");
+                string command = Console.ReadLine();
+                int nrcitit = int.Parse(Console.ReadLine());
+                switch(command)
+                {
+                    case "A":
+                        Console.WriteLine(nr>=nrcitit);
+                        break;
+                    case "B":
+                        Console.WriteLine(nr%nrcitit);
+                        break;
+                    case "R":
+                        Console.WriteLine(nr==nrcitit);
+                        reia = !(nr == nrcitit);
+                        break;
+                }
+            }
+        }
+    }
+}
